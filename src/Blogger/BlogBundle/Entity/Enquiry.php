2a9a39c41893c87025acf631e9dfbfc502603bcb
karmeljuk <karmeljuk@gmail.com>
2013-12-08 16:16:56 +0200

    flash message

--- src/Blogger/BlogBundle/Entity/Enquiry.php
+++ src/Blogger/BlogBundle/Entity/Enquiry.php
@@ -2,6 +2,11 @@
 
 namespace Blogger\BlogBundle\Entity;
 
+use Symfony\Component\Validator\Mapping\ClassMetadata;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+
 class Enquiry
 {
     protected $name;
@@ -50,5 +55,19 @@ class Enquiry
     public function setBody($body)
     {
         $this->body = $body;
+    }
+
+    public static function loadValidatorMetadata(ClassMetadata $metadata)
+    {
+        $metadata->addPropertyConstraint('name', new NotBlank());
+
+        $metadata->addPropertyConstraint('email', new Email(array(
+            'message' => 'symblog does not like invalid emails. Give me a real one!'
+        )));
+
+        $metadata->addPropertyConstraint('subject', new NotBlank());
+        $metadata->addPropertyConstraint('subject', new Length(array('max'=>50)));
+
+        $metadata->addPropertyConstraint('body', new Length(array('min'=>50)));
     }
 }
